a054d783fde5fa866d9a55c7fa30499ec3d2fb52
Michelle Tan <mtan5334@uw.edu>
2017-02-07 15:17:11 -0800

    Add stat transforms and position adjustments

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,35 +1,40 @@
-# Exercise 2: More ggplot2 Grammar
+# MODULE 13 Exercise 2: More ggplot2 Grammar
 
 # Install and load `ggplot2`
 # install.packages("ggplot2") # if needed
-
+library("ggplot2")
 
 # For this exercise you will again be working with the `diamonds` data set.
 # Use `?diamonds` to review details about this data set
-
+?diamonds
 
 
 ## Statistical Transformations
 
 # Draw a bar chart of the diamonds data, organized by cut
 # The height of each bar is based on the "count" (number) of diamonds with that cut
-
+ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x = cut))
 
 # Use the `stat_count` to apply the statistical transformation "count" to the diamonds
 # by cut. You do not need a separate geometry layer!
-
+ggplot(data = diamonds) +
+  stat_count(mapping = aes(x=cut)) #same graph but stat allows for statistical transformation to calculate the data
 
 # Use the `stat_summary` function to draw a chart with a summary layer.
 # Map the x-position to diamond `cut`, and the y-position to diamond `depth`
 # Bonus: use `min` as the function ymin, `max` as the function ymax, and `median` as the function y
-
+ggplot(data = diamonds) +
+  stat_summary(mapping = aes(x = cut, y = depth),
+              fun.ymin = min, fun.ymax = max, fun.y = median) #adds range to the dots?
 
 
 ## Position Adjustments
 
-# Draw a bar chart of diamond data organized by cut, with each bar filled by clarity.
+# Draw a bar chart of diamond data organized by cut, with each bar filled by clarity (coloring in bars)
 # You should see a _stacked_ bar chart.
-
+ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x = cut, fill = clarity))
 
 # Draw the same chart again, but with each element positioned to "fill" the y axis
 
